9b5deea4649d36d05ecdbec160a33400e02458ca
Baptiste Lemonnier <baptiste.lmn@gmail.com>
2023-11-02 19:35:48 +0100

    config ok

--- time_manager/entrypoint.sh
+++ time_manager/entrypoint.sh
@@ -1,32 +1,25 @@
 #!/bin/bash
 
-# Naviguer dans le répertoire du projet
 cd /app
 
-# Créer et migrer la base de données
 mix ecto.setup
 
-# Attente de la disponibilité de la base de données
+# waiting the database
 while ! pg_isready -h $PGHOST -p $PGPORT -q -U $PGUSER
 do
-  echo "$(date) - en attente de la base de données..."
+  echo "$(date) - waiting for database..."
   sleep 1
 done
+echo ----------------------------------
+echo "Database is ready !"
+echo ----------------------------------
 
-echo "La base de données est prête !"
-
-# Créer la base de données (ne fait rien si elle existe déjà)
 mix ecto.create
 
-# Exécuter les migrations de la base de données
 mix ecto.migrate
 
-# Démarrer le serveur Phoenix
 mix phx.server
-#!/bin/bash
 
-# Naviguer dans le répertoire du projet
 cd /app
 
-# Installer les dépendances
 mix deps.get
